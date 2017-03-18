4c641ac68fbd55c756916d0387a05375bad22af1
iin-indriyani <iinindriyani286@gmail.com>
2017-03-18 10:24:49 -0700

    first commit

---
+++ InputOutput1.java
@@ -0,0 +1,7 @@
+public class InputOutput1 {
+	public static void main (String [] args) {
+		System.out.print("Belajar Java Yuks 1");
+		System.out.println("Belajar Java Yuks 2");
+		System.out.print("Belajar Java Yuks3");
+	}
+}
